8ba1a71ed2f1e9176bf443209580523bfac0841b
mahendras.sonawane <mahendras.sonawane@esds.co.in>
2024-01-17 11:53:06 +0530

    Edit Done

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'employees',
         'USER': 'root',
-        'PASSWORD': 'dbda',
+        'PASSWORD': 'esds',
         'HOST': 'localhost',
         'PORT': '3306',
     }
