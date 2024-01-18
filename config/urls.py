48b99c2ee87313a87a2af1baa459ee28cadbf918
mahendras.sonawane <mahendras.sonawane@esds.co.in>
2024-01-18 14:31:20 +0530

    URL

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('index',render())
-    path('', include('employees.urls'))
+    # path('', include('employees.urls'))
+    path('', lambda request: redirect('/employees')),
+    path('', include('employees.urls')),
 ]
